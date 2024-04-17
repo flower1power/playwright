d9f6f2bc03a106690c343fd57c44e03399979324
Виктор Лобынцев <sibfight@mail.ru>
2024-04-17 20:08:28 +0300

    getText

---
+++ tests/lesson3_getText.spec.js
@@ -0,0 +1,13 @@
+const { test, expect } = require("@playwright/test");
+
+test("getText", async ({ page }) => {
+  await page.goto("https://inzhenerka.tech/");
+
+  // await page.locator('[field=descr]')
+  await expect(await page.locator("[field=descr]").first()).toHaveText(
+    "Помогаем инженерам повысить свою квалификацию на рынке труда и приобрести навыки международного уровня"
+  );
+  await expect(await page.locator("[field=title]").nth(1)).toContainText(
+    "ИнженеркаТех"
+  );
+});
